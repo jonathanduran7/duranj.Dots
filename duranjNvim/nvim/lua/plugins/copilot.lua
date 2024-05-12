b88c40cfa9d654b2849d6f693ccca1fa2f5d4616
jonathanduran7 <joniduran624@gmail.com>
2024-05-12 15:11:42 -0300

    add copilot

---
+++ duranjNvim/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-cmp",
+  dependencies = {
+    -- Copilot
+    {
+      "github/copilot.vim"
+    },
+  },
+  ---@param opts cmp.ConfigSchema
+  opts = function(_, opts)
+    table.insert(opts.sources, 1, {
+      name = "Copilot",
+      group_index = 1,
+      priority = 100,
+    })
+  end,
+}
