b61326fd3cc2cf90b358a3110efa86d761c88b51
jduran <jduran@gen-it.com.ar>
2024-05-23 23:02:46 -0300

    update configs

--- duranjNvim/nvim/lua/plugins/git-stuff.lua
+++ duranjNvim/nvim/lua/plugins/git-stuff.lua
@@ -1,9 +1,28 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  config = function()
-    require("gitsigns").setup()
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
 
-    vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-    vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
-  end,
+      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+    end,
+  },
+  {
+    "ThePrimeagen/git-worktree.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {},
+    config = function()
+      require("telescope").load_extension("git_worktree")
+    end,
+    -- stylua: ignore
+    keys = {
+      {"<leader>gw", function() require("telescope").extensions.git_worktree.git_worktrees() end, desc = "Git worktress"},
+      {"<leader>gwc", function() require("telescope").extensions.git_worktree.create_git_worktree() end, desc = "Create"},
+    }
+,
+  },
 }
